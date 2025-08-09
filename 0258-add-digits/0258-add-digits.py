68edfa759cf54cb27748ff273c706b569225fade
Prathithi M L <pranathiprathithi@gmail.com>
2025-08-09 06:57:41 +0530

    Time: 0 ms (100%), Space: 17.6 MB (86.94%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,13 @@
+class Solution:
+    def SumOfDigits(num):
+        summ = 0
+        for i in str(num):
+            summ += int(i)
+        return summ
+
+    def addDigits(self, num: int) -> int:
+        while len(str(num)) != 1:
+            num = Solution.SumOfDigits(num)
+        return num
+
+        
