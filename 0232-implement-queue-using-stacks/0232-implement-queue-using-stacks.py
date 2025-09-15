d2e453e8ffe6092dafc999de1652a2fcd93817ac
Prathithi M L <pranathiprathithi@gmail.com>
2025-09-15 07:19:03 +0530

    Time: 0 ms (100%), Space: 18 MB (38.89%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -20,7 +20,7 @@ class MyQueue:
         return self.s2[-1]
 
     def empty(self) -> bool:
-        return not self.s1 and not self.s2
+        return max(len(self.s1), len(self.s2)) ==0
 
         
 
