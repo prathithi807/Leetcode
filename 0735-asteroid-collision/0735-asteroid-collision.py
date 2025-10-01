6d6a649d1f7ca6feaf75f54ac5af076f8c4128f1
Prathithi M L <pranathiprathithi@gmail.com>
2025-10-01 10:15:09 +0530

    Time: 0 ms (100%), Space: 19.1 MB (19.95%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -4,9 +4,9 @@ class Solution:
         for a in asteroids:
             while stack and a<0 and stack[-1]>0:
                 diff = a + stack[-1]
-                if diff>0:
+                if diff<0:
                     stack.pop()
-                elif diff<0:
+                elif diff>0:
                     a=0
                 else:
                     a=0
